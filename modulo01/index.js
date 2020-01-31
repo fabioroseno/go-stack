b88ed9b1fb356e8d8685d1f80f85d1b3d2eb45e3
Fábio Roseno <fabiorosenos@gmail.com>
2020-01-31 00:58:41 +0000

    Utilizando Nodemon

---
+++ modulo01/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+const users = ['Diego', 'Fábio', 'Nanque'];
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+})
+
+server.listen(3333);
